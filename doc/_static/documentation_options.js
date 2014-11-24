e58c6d88454404a4baa23377c53d419f87fdd2eb
Stewart Smith <stewart@linux.vnet.ibm.com>
2014-11-24 15:50:05 +1100

    Skiboot documentation as of v6.2 and v6.0.15
    Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

---
+++ doc/_static/documentation_options.js
@@ -0,0 +1,9 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: 'v6.2-1-g06ef9bd2d9a6',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt'
+};
